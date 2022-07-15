6cfadd8085f3cb1e48be54c603370355f30ccdc5
Bruno Bastos <brunojbastos@yahoo.com.br>
2022-07-15 14:32:59 -0300

    Version final 01

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ onScroll()
 function onScroll() {
     showNavOnScroll();
     showBackToTopButtonOnScroll();
+
+    activateMenuAtCurrentSection(home);
+    activateMenuAtCurrentSection(services);
+    activateMenuAtCurrentSection(about);
+    activateMenuAtCurrentSection(contact);
 }
 
+function activateMenuAtCurrentSection(section) {
+    const targetLine = scrollY + innerHeight / 2;
+
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+    
+    const sectionEndsAt = sectionTop + sectionHeight;
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+    menuElement.classList.remove('active');
+    if (sectionBoundaries) {
+        menuElement.classList.add('active');
+
+    }
+}
+ 
 function showNavOnScroll() {
     const navigation = document.getElementById('navigation');
     if(scrollY > 0) {
@@ -45,4 +72,8 @@ ScrollReveal({
 #services .card,
 #about,
 #about header,
-#about content`);
+#about content,
+#contact,
+#contact header,
+#contact content,
+footer`);
